e82ee8f25c38bb60c83382c4d383b18b0ce1a32b
Royal Dev <ajaiplayer990@gmail.com>
2025-07-03 07:48:11 -0700

    fix: test issue

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
